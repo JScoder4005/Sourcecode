3765b047160f225e34f6351cd77bfd14348a2f79
Uday kumar M <136188719+JScoder4005@users.noreply.github.com>
2024-10-04 17:40:37 +0530

    Create revString.js

---
+++ revString.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+    let reversed = '';
+    
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i]; 
+    }
+    
+    return reversed;
+}
+
+// Example usage:
+let originalString = "Hello, World!";
+let reversedString = reverseString(originalString);
+console.log(reversedString);
